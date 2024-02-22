01be50cc5b0eb108a353584c4893037a20a94eb9
22DP1DVasi <131352712+22DP1DVasi@users.noreply.github.com>
2024-02-22 18:45:47 +0200

    Spaceship done

--- Spaceship.cs
+++ Spaceship.cs
@@ -8,12 +8,12 @@ class Spaceship{
     private double coorZ;                       // kosmosa kuģa koordināta Z
     private bool isFlying;                      // vai kosmosa kuģis tagad lido
     private double acceleration;                // kosmosa kuģa paātrinājums. Var būt gan pozitīvs (kuģis lido uz priekšu), gan negatīvs (kuģis lido atpakaļ)
-    private uint passengers;                    // kosmosa kuģa pasažieru skaits (minimums - 0; maksimums - 50). Šis lauks nevar būt negatīvs
-    private uint oxygenPercentage;              // kosmosa kuģa skābekļa procentuālais daudzums (minimums - 0, maksimums - 100)
+    private int passengers;                    // kosmosa kuģa pasažieru skaits (minimums - 0; maksimums - 50). Šis lauks nevar būt negatīvs
+    private int oxygenPercentage;              // kosmosa kuģa skābekļa procentuālais daudzums (minimums - 0, maksimums - 100)
     private static int spaceshipCount = 0;      // statiskais izveidoto objektu skaitītājs
     
     // konstruktors ar visiem parametriem
-    public Spaceship(string theModel, string theName, double theCoorX, double theCoorY, double theCoorZ, bool theIsFlying, double theAcceleration, uint thePassengers, uint theOxygenPercentage)
+    public Spaceship(string theModel, string theName, double theCoorX, double theCoorY, double theCoorZ, bool theIsFlying, double theAcceleration, int thePassengers, int theOxygenPercentage)
     {
         this.model = theModel;
         this.name = theName;
@@ -22,7 +22,7 @@ class Spaceship{
         this.coorZ = theCoorZ;
         this.isFlying = theIsFlying;
         this.acceleration = theAcceleration;
-        if (thePassengers > 50)    // maksimālais pasažieru skaits ir 50, minimālais - 0. Ja ievadīta vērtība ir nepareiza, passengers lauks tiek iestatīts uz 0
+        if (thePassengers > 50 || thePassengers < 0)    // maksimālais pasažieru skaits ir 50, minimālais - 0. Ja ievadīta vērtība ir nepareiza, passengers lauks tiek iestatīts uz 0
         {
             Console.WriteLine("Nepareizi ievadīts kosmosa kuģa pasažieru skaits (min = 0, max = 50). Pasažieru skaits uz kosmosa kuģa ir iestatīts uz 0.");
             this.passengers = 0;
@@ -32,7 +32,7 @@ class Spaceship{
             this.passengers = thePassengers;
         }
         
-        if (theOxygenPercentage > 100)   // maksimālais skābekļa procentuālais daudzums ir 100, minimālais - 0. Ja ievadīta vērtība ir nepareiza, oxygenPercentage lauks tiek iestatīts uz 100
+        if (theOxygenPercentage > 100 || theOxygenPercentage < 0)   // maksimālais skābekļa procentuālais daudzums ir 100, minimālais - 0. Ja ievadīta vērtība ir nepareiza, oxygenPercentage lauks tiek iestatīts uz 100
         {
             Console.WriteLine("Nepareizi ievadīts kosmosa kuģa skābekļa procentuālais daudzums (min = 0, max = 100). Kosmosa kuģa skābekļa procentuālais daudzums ir iestatīts uz 100.");
             this.oxygenPercentage = 100;
@@ -102,7 +102,7 @@ class Spaceship{
         set { this.acceleration = value; }
     }
     
-    public uint Passengers // passengers property
+    public int Passengers // passengers property
     {
         get { return this.passengers; }
         set
@@ -118,7 +118,7 @@ class Spaceship{
         }
     }
     
-    public uint OxygenPercentage // oxygenPercentage property
+    public int OxygenPercentage // oxygenPercentage property
     {
         get { return this.oxygenPercentage; }
         set
@@ -154,27 +154,59 @@ class Spaceship{
         this.coorZ += distance;
     }
     
-    public void RemovePassangers (uint amount)       // izvest no kosmosa kuģa noteiktu skaitu pasažieru
+    public void RemovePassengers (int amount)       // izvest no kosmosa kuģa noteiktu skaitu pasažieru
     {   
         if (this.passengers < amount)       // ja norādīta vērtība lielāka par iestādīto vērtību, aizliegt darbību
         {
             Console.WriteLine("Nav iespējams. Norādītais cilvēku skaits ir lielāks nekā pasažieru skaits uz kosmosa kuģa.");
         }
+        else if (amount < 0)    // ja norādīta vērtība ir negatīva
+        {
+            Console.WriteLine("Nav iespējams. Vērtībai jābūt vienādai ar vai lielākai par nulli.");
+        }
         else
         {
             this.passengers -= amount;
+            Console.WriteLine("Izvest pasažierus: Darbība ir veiksmīga.");
         }
     }
     
-    public void AddPassengers (uint amount)     // pievienot kosmosa kuģim pasažierus
+    public void AddPassengers (int amount)     // pievienot kosmosa kuģim pasažierus
     {
         if (this.passengers + amount > 50)      // ja kopējā vērtība lielāka par maksimālo vērtību 50, aizliegt darbību
         {
             Console.WriteLine("Nav iespējams. Kopējā vērtība pārsniedz maksimālo iespējamo pasažieru skaitu kosmosa kuģī (50 cilvēki).");
         }
+        else if (amount < 0)    // ja norādīta vērtība ir negatīva, aizliegt darbību
+        {
+            Console.WriteLine("Nav iespējams. Vērtībai jābūt vienādai ar vai lielākai par nulli.");
+        }
         else
         {
             this.passengers += amount;
+            Console.WriteLine("Pievienot pasažierus: Darbība ir veiksmīga.");
         }
+    }
+    
+    public void AddOxygen (int percents)    // pievienot skābekli procentuāli
+    {
+        if (this.oxygenPercentage + percents > 100)     // ja kopējā vērtība lielāka par maksimālo vērtību 100, aizliegt darbību
+        {
+            Console.WriteLine("Nav iespējams. Kopējā vērtība pārsniedz maksimālo iespējamo skābekļa procentuālo daudzumu kosmosa kuģī (100 %)");
+        }
+        else if (percents < 0)      // ja norādīta vērtība ir negatīva, aizliegt darbību
+        {
+            Console.WriteLine("Nav iespējams. Vērtībai jābūt vienādai ar vai lielākai par nulli.");
+        }
+        else
+        {
+            this.oxygenPercentage += percents;
+            Console.WriteLine("Pievienot skābekli procentuāli: Darbība ir veiksmīga.");
+        }
+    }
+    
+    public void ChangeAccelerByValue (double val)   // mainīt paātrinājumu uz doto vērtību (val > 0: paaugstināt; val < 0: samazināt)
+    {
+        this.acceleration += val;
     }
 }
--- main.cs
+++ main.cs
@@ -2,7 +2,7 @@ using System;
 class Program {
   static void Main() {
     // izveido pirmo objektu
-    Spaceship spcship1 = new Spaceship("SpaceX", "Arbiter", 243.52, 565.24, -845.30, true, 1100.0, 50, 100);
+    Spaceship spcship1 = new Spaceship("SpaceX", "Arbiter", 243.52, 565.24, -845.30, true, 11000, 50, 50);
     Spaceship spcship2 = new Spaceship();
     
     // Paradām konsolē info par pirmo objektu
@@ -25,6 +25,47 @@ class Program {
         );
     Console.WriteLine("");
     
-    spcship1.WarpAlongCoorX(-100);
+    // Nomainīt otra kosmosa kuģa modeli un vārdu
+    spcship2.Model = "Galax";
+    spcship2.Name = "Oppy";
+    
+    // Nomainīt pirma kosmosa kuģa koordināti X par 100 un koordināti Z par -200
+    spcship1.WarpAlongCoorX(100);
+    spcship1.WarpAlongCoorZ(-200);
+    
+    // Izvest 20 pasažierus no otra kosmosa kuģa
+    spcship1.RemovePassengers(20);
+    
+    // Pievienot 25% skābekļa pirmam kosmosa kuģim
+    spcship1.AddOxygen(25);
+    
+    // Atņemt no pirma kosmosa kuģa paātrinājuma 1000.55 vienības
+    spcship1.ChangeAccelerByValue(-1000.55);
+    
+    Console.WriteLine("");
+    
+    // Paradīt, cik objekti bija izveidoti
+    Console.WriteLine("Total spaceships: {0}", Spaceship.SpaceshipCount);
+    Console.WriteLine("");
+    
+    // Paradām konsolē info par pirmo objektu
+    Console.WriteLine(
+        @"#1: Model = {0}, Name = {1},
+    X: {2}, Y: {3}, Z: {4},
+    Is spaceship flying: {5}, Acceleration = {6}, Passengers = {7}, Percentage of oxygen = {8}",
+        spcship1.Model, spcship1.Name, spcship1.CoorX, spcship1.CoorY, spcship1.CoorZ,
+        spcship1.IsFlying, spcship1.Acceleration, spcship1.Passengers, spcship1.OxygenPercentage
+        );
+    Console.WriteLine("");
+    
+    // Paradām konsolē info par otro objektu
+    Console.WriteLine(
+        @"#2: Model = {0}, Name = {1},
+    X: {2}, Y: {3}, Z: {4},
+    Is spaceship flying: {5}, Acceleration = {6}, Passengers = {7}, Percentage of oxygen = {8}",
+        spcship2.Model, spcship2.Name, spcship2.CoorX, spcship2.CoorY, spcship2.CoorZ,
+        spcship2.IsFlying, spcship2.Acceleration, spcship2.Passengers, spcship2.OxygenPercentage
+        );
+    Console.WriteLine("");
   }
 }
